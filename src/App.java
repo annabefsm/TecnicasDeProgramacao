a0e93173b291e72fae517ebf9ad9794effc3fa4d
annabefsm <69550398+annabefsm@users.noreply.github.com>
2020-10-20 20:11:30 -0300

    update
    como eu fiz

--- src/App.java
+++ src/App.java
@@ -14,3 +14,34 @@ public class App {
         user.printaUsuario();
     }
 }
+//import java.util.Scanner;
+
+import model.Entregador;
+
+public class principal {
+	public void main (String[]args) {
+		Entregador entregador= new Entregador();
+		entregador= setarDadosEntregador();
+		
+	}
+	public static Entregador setarDadosEntregador() {
+		Entregador entregador= new Entregador();
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Digite o nome do entregador:");
+		entregador.setNome(scan.nextLine());
+		System.out.println("Digite um endereço:");
+		entregador.setEndereco(scan.next());
+		System.out.println("Digite o telefone:");
+		entregador.setTelefone(scan.next());
+		System.out.println(" \n");
+		return entregador;
+	}
+	public void printarPessoa(Entregador entregador) {
+		// TODO Auto-generated method stub
+		System.out.println("Os dados do entregador são:");
+		System.out.println("Nome: " +entregador.getNome());
+		System.out.println("Endereço" +entregador.getEndereco());
+		System.out.println("Telefone: " +entregador.getTelefone());
+		System.out.println(" \n");
+	}
+}
